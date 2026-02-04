34f6c00eaaf04015271041d398ff78957b78b28d
zikou058 <rajawizakaria546@gmail.com>
2026-02-04 15:12:56 +0100

    config2

---
+++ script1.js
@@ -0,0 +1,10 @@
+// page 4 
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('hireme23').addEventListener("click", () => {
+        hireme23();
+    });
+});
+const hireme23 = () => {
+    window.location.href = "contact.html";
+}
+ 
